7a5b28f953471774bada4ce86ed61450456d9648
Ayomide <ilesanmibry@gmail.com>
2023-12-05 10:56:51 +0000

    commit 3-4

--- models/__init__.py
+++ models/__init__.py
@@ -3,10 +3,13 @@
 from os import getenv
 
 
+# Create a single instance of DBStorage
 if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+
+# Reload the storage after creating the instance
 storage.reload()
