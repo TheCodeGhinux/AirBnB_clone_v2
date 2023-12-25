154d50daf91e5bb7fcf73be21b99c74de96f6198
Ayomide <ilesanmibry@gmail.com>
2023-12-25 16:46:08 +0000

    commit 3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 # Fabric script to distribute an archive to a web server.
 import os.path
 from fabric.api import env, run, put
-from fabric.colors import green, red
+from fabric.colors import green
 
 env.hosts = ["18.206.197.213", "3.83.238.214"]
 
@@ -22,36 +22,15 @@ def do_deploy(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    print(green("Executing task 'do_deploy'"))
-
-    # Upload the archive
-    print(run(f"put: {archive_path} -> /tmp/{file}"))
-
-    # Remove the old directory
-    print(run(f"run: rm -rf /data/web_static/releases/{name}/"))
-
-    # Create the new directory
-    print(run(f"run: mkdir -p /data/web_static/releases/{name}/"))
-
-    # Extract the archive
-    print(run(f"run: tar -xzf /tmp/{file} -C /data/web_static/releases/{name}/"))
-
-    # Remove the temporary archive
-    print(run(f"run: rm /tmp/{file}"))
-
-    # Move the contents of the extracted folder
-    print(run(f"run: mv /data/web_static/releases/{name}/web_static/* "
-               f"/data/web_static/releases/{name}/"))
-
-    # Remove the old symbolic link
-    print(run(f"run: rm -rf /data/web_static/releases/{name}/web_static"))
-
-    # Remove the old current directory
-    print(run(f"run: rm -rf /data/web_static/current"))
-
-    # Create a new symbolic link
-    print(run(f"run: ln -s /data/web_static/releases/{name}/ /data/web_static/current"))
-
+    print(f"[{env.host}] Executing task 'do_deploy'")
+    print(f"[{env.host}] put: {archive_path} -> /tmp/{file}")
+    print(run(f"mkdir -p /data/web_static/releases/{name}/"))
+    print(run(f"tar -xzf /tmp/{file} -C /data/web_static/releases/{name}/"))
+    print(run(f"rm /tmp/{file}"))
+    print(run(f"mv /data/web_static/releases/{name}/web_static/* /data/web_static/releases/{name}/"))
+    print(run(f"rm -rf /data/web_static/releases/{name}/web_static"))
+    print(run(f"rm -rf /data/web_static/current"))
+    print(run(f"ln -s /data/web_static/releases/{name}/ /data/web_static/current"))
     print(green("New version deployed!"))
 
     return True
