3342a9a640ceb68ab4aac1dad275159421cfc1f9
Ayomide <ilesanmibry@gmail.com>
2023-12-25 16:42:35 +0000

    commit 3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 # Fabric script to distribute an archive to a web server.
 import os.path
-from fabric.api import env
-from fabric.api import run
-from fabric.api import put
+from fabric.api import env, run, put
+from fabric.colors import green, red
 
 env.hosts = ["18.206.197.213", "3.83.238.214"]
 
@@ -19,31 +18,40 @@ def do_deploy(archive_path):
     """
     if os.path.isfile(archive_path) is False:
         return False
+
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
+    print(green("Executing task 'do_deploy'"))
+
+    # Upload the archive
+    print(run(f"put: {archive_path} -> /tmp/{file}"))
+
+    # Remove the old directory
+    print(run(f"run: rm -rf /data/web_static/releases/{name}/"))
+
+    # Create the new directory
+    print(run(f"run: mkdir -p /data/web_static/releases/{name}/"))
+
+    # Extract the archive
+    print(run(f"run: tar -xzf /tmp/{file} -C /data/web_static/releases/{name}/"))
+
+    # Remove the temporary archive
+    print(run(f"run: rm /tmp/{file}"))
+
+    # Move the contents of the extracted folder
+    print(run(f"run: mv /data/web_static/releases/{name}/web_static/* "
+               f"/data/web_static/releases/{name}/"))
+
+    # Remove the old symbolic link
+    print(run(f"run: rm -rf /data/web_static/releases/{name}/web_static"))
+
+    # Remove the old current directory
+    print(run(f"run: rm -rf /data/web_static/current"))
+
+    # Create a new symbolic link
+    print(run(f"run: ln -s /data/web_static/releases/{name}/ /data/web_static/current"))
+
+    print(green("New version deployed!"))
+
     return True
