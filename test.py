496f5c45b4bb213fc599d4cdd375a6d96878b652
Ayomide <ilesanmibry@gmail.com>
2023-12-25 16:10:33 +0000

    commit 2

--- test.py
+++ test.py
@@ -1,32 +1,18 @@
+class Base():
+    """ My base class """
 
+    __nb_instances = 0
 
+    def __init__(self):
+        Base.__nb_instances += 1
+        self.id = Base.__nb_instances
 
-        for param in args_list[1:]:
-            key, value = param.split('=')
+class User(Base):
+    """ My User class """
 
-            """This checks if the value starts and ends with double quotes"""
-            if value.startswith('"') and value.endswith('"'):
-                """ To Remove the quotes and replace underscores with spaces"""
-                value = value[1:-1].replace('_', ' ')
-            elif '.' in value:
-                """Check if value is a float"""
-                try:
-                    value = float(value)
-                except ValueError:
-                    print(f"Invalid value for {key}: {value}")
-                    return
-            elif value.isdigit():
-                """Check if value is a int"""
-                value = int(value)
+    def __init__(self):
+        super().__init__()
+        self.id += 99
 
-            """Adds key-value pair to dict"""
-            params[key] = value
-
-        """Creates a new instance """
-        new_inst = HBNBCommand.classes[class_name](**params)
-
-        # Save the new instance
-        storage.save()
-
-        print(new_inst.id)
-        storage.save()
+u = User()
+print(u.id)
